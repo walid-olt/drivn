a17e78c2e8fc7fb8141050f7e13ab21ed98b46d3
walid-hud <walidthehud@gmail.com>
2026-06-19 17:59:36 +0100

    wip:setup

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@ui/button";
+import { RocketLaunchIcon } from "@phosphor-icons/react";
+const App = () => {
+  return (
+    <div>
+      <Button>
+        {" "}
+        let's go <RocketLaunchIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default App;
